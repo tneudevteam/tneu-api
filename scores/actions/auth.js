f62d409717a0cb51b314789c8c34c00f0d6fef11
Vlad Golubev <golubev.vld@gmail.com>
2016-03-19 18:12:03 +0200

    Implement recieving access token

--- scores/actions/auth.js
+++ scores/actions/auth.js
@@ -1,10 +1,46 @@
+const rest = require('request-promise');
+const cookie = require('cookie');
+const _ = require('lodash');
+
 function auth(message, done) {
     const username = message.username;
     const password = message.password;
 
-    console.log('[auth]', username, password);
+    console.log('[auth] user:', username);
 
-    done(null, { success: true });
+    _requestAuthToken(username, password)
+        .then((response) => {
+            done(null, { success: true, token: response });
+        })
+        .catch((error) => {
+            done(null, { success: false, error: error });
+        });
+}
+
+function _requestAuthToken(username, password) {
+    const options = {
+        uri: 'http://mod.tanet.edu.te.ua/site/login',
+        method: 'POST',
+        resolveWithFullResponse: true,
+        simple: false,
+        form: {
+            'LoginForm[login]': username,
+            'LoginForm[password]': password,
+            'LoginForm[rememberMe]': '1',
+            yt0: 'Увійти'
+        },
+        jar: true,
+        transform: function (body, response, resolveWithFullResponse) {
+            if (resolveWithFullResponse) {
+                const cookies = _.map(response.headers['set-cookie'], (one) => cookie.parse(one));
+                return _.get(_.findLast(cookies, (el) => _.has(el, 'PHPSESSID')), 'PHPSESSID');
+            } else {
+                return body;
+            }
+        }
+    };
+
+    return rest(options);
 }
 
 module.exports = auth;
