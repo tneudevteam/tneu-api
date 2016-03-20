c4be46716adeb1d0723945043021f6e0706e34da
Vlad Golubev <golubev.vld@gmail.com>
2016-03-20 13:34:13 +0200

    Fix listening port for heroku deployment

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const seneca = require('seneca');
 
 seneca()
     .use('./scores')
-    .listen({ host: 'localhost', port: '9101' });
+    .listen({ host: '0.0.0.0', port: process.env.PORT || 9101 });
